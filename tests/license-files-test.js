c8aec838de1bd039ba93fa45a5cf02d280ad345d
Roman Seidelsohn <rseidelsohn@gmail.com>
2021-04-08 18:06:16 +0200

    Apply prettier formatting and use const rather than var

--- tests/license-files-test.js
+++ tests/license-files-test.js
@@ -1,41 +1,25 @@
-var assert = require('assert'),
-    licenseFiles = require('../lib/license-files');
+const assert = require('assert');
+const licenseFiles = require('../lib/license-files');
 
-describe('license files detector', function() {
-
-    it('should export a function', function() {
+describe('license files detector', function () {
+    it('should export a function', function () {
         assert.equal(typeof licenseFiles, 'function');
     });
 
-    it('no files', function() {
+    it('no files', function () {
         assert.deepEqual(licenseFiles([]), []);
     });
 
-    it('no license files', function() {
-        assert.deepEqual(licenseFiles([
-            '.gitignore',
-            '.travis.yml',
-            'TODO',
-        ]), []);
+    it('no license files', function () {
+        assert.deepEqual(licenseFiles(['.gitignore', '.travis.yml', 'TODO']), []);
     });
 
-    it('one license candidate', function() {
-        assert.deepEqual(licenseFiles([
-            'LICENSE',
-            '.gitignore',
-            'src',
-        ]), ['LICENSE']);
+    it('one license candidate', function () {
+        assert.deepEqual(licenseFiles(['LICENSE', '.gitignore', 'src']), ['LICENSE']);
     });
 
-    it('multiple license candidates detected in the right order', function() {
-        assert.deepEqual(licenseFiles([
-            'COPYING',
-            '.gitignore',
-            'LICENCE',
-            'LICENSE',
-            'src',
-            'README',
-        ]), [
+    it('multiple license candidates detected in the right order', function () {
+        assert.deepEqual(licenseFiles(['COPYING', '.gitignore', 'LICENCE', 'LICENSE', 'src', 'README']), [
             'LICENSE',
             'LICENCE',
             'COPYING',
@@ -43,45 +27,19 @@ describe('license files detector', function() {
         ]);
     });
 
-    it('extensions have no effect', function() {
-        assert.deepEqual(licenseFiles([
-            'LICENCE.txt',
-            '.gitignore',
-            'src',
-        ]), [
-            'LICENCE.txt',
-        ]);
+    it('extensions have no effect', function () {
+        assert.deepEqual(licenseFiles(['LICENCE.txt', '.gitignore', 'src']), ['LICENCE.txt']);
     });
 
-    it('lower/upper case has no effect', function() {
-        assert.deepEqual(licenseFiles([
-            'LiCeNcE',
-            '.gitignore',
-            'src',
-        ]), [
-            'LiCeNcE',
-        ]);
+    it('lower/upper case has no effect', function () {
+        assert.deepEqual(licenseFiles(['LiCeNcE', '.gitignore', 'src']), ['LiCeNcE']);
     });
 
-    it('LICENSE-MIT gets matched', function() {
-        assert.deepEqual(licenseFiles([
-            'LICENSE',
-            '.gitignore',
-            'LICENSE-MIT',
-            'src',
-        ]), [
-            'LICENSE',
-            'LICENSE-MIT',
-        ]);
+    it('LICENSE-MIT gets matched', function () {
+        assert.deepEqual(licenseFiles(['LICENSE', '.gitignore', 'LICENSE-MIT', 'src']), ['LICENSE', 'LICENSE-MIT']);
     });
 
-    it('only the first LICENSE-* file gets matched', function() {
-        assert.deepEqual(licenseFiles([
-            'license-foobar.txt',
-            '.gitignore',
-            'LICENSE-MIT',
-        ]), [
-            'license-foobar.txt',
-        ]);
+    it('only the first LICENSE-* file gets matched', function () {
+        assert.deepEqual(licenseFiles(['license-foobar.txt', '.gitignore', 'LICENSE-MIT']), ['license-foobar.txt']);
     });
 });
--- tests/license.js
+++ tests/license.js
@@ -1,13 +1,13 @@
-var assert = require('assert'),
-    util = require('util'),
-    license = require('../lib/license');
+const assert = require('assert');
+const util = require('util');
+const license = require('../lib/license');
 
-describe('license parser', function() {
-    it('should export a function', function() {
+describe('license parser', function () {
+    it('should export a function', function () {
         assert.equal(typeof license, 'function');
     });
 
-    it('should throw an error when called with a non-string argument', function(done) {
+    it('should throw an error when called with a non-string argument', function (done) {
         try {
             license({});
         } catch (err) {
@@ -16,31 +16,31 @@ describe('license parser', function() {
         }
     });
 
-    it('removes newlines from the argument', function() {
+    it('removes newlines from the argument', function () {
         assert.equal(license('unde\nfined'), 'Undefined');
     });
 
-    it('undefined check', function() {
+    it('undefined check', function () {
         assert.equal(license(undefined), 'Undefined');
     });
 
-    it('MIT check', function() {
-        var data = license('asdf\nasdf\nasdf\nPermission is hereby granted, free of charge, to any');
+    it('MIT check', function () {
+        const data = license('asdf\nasdf\nasdf\nPermission is hereby granted, free of charge, to any');
         assert.equal(data, 'MIT*');
     });
 
-    it('MIT word check', function() {
-        var data = license('asdf\nasdf\nMIT\nasdf\n');
+    it('MIT word check', function () {
+        const data = license('asdf\nasdf\nMIT\nasdf\n');
         assert.equal(data, 'MIT*');
     });
 
-    it('Non-MIT word check', function() {
-        var data = license('prefixMIT\n');
+    it('Non-MIT word check', function () {
+        const data = license('prefixMIT\n');
         assert.notEqual(data, 'MIT*');
     });
 
-    it('GPL word check', function() {
-        var data;
+    it('GPL word check', function () {
+        let data;
         data = license('GNU GENERAL PUBLIC LICENSE \nVersion 1, February 1989');
         assert.equal(data, 'GPL-1.0*');
         data = license('GNU GENERAL PUBLIC LICENSE \nVersion 2, June 1991');
@@ -49,8 +49,8 @@ describe('license parser', function() {
         assert.equal(data, 'GPL-3.0*');
     });
 
-    it('Non-GPL word check', function() {
-        var data;
+    it('Non-GPL word check', function () {
+        let data;
         data = license('preGNU GENERAL PUBLIC LICENSE \nVersion 1, February 1989');
         assert.notEqual(data, 'GPL-1.0*');
         data = license('preGNU GENERAL PUBLIC LICENSE \nVersion 2, June 1991');
@@ -59,8 +59,8 @@ describe('license parser', function() {
         assert.notEqual(data, 'GPL-3.0*');
     });
 
-    it('LGPL word check', function() {
-        var data;
+    it('LGPL word check', function () {
+        let data;
         data = license('GNU LIBRARY GENERAL PUBLIC LICENSE\nVersion 2, June 1991');
         assert.equal(data, 'LGPL-2.0*');
         data = license('GNU LESSER GENERAL PUBLIC LICENSE\nVersion 2.1, February 1999');
@@ -69,82 +69,82 @@ describe('license parser', function() {
         assert.equal(data, 'LGPL-3.0*');
     });
 
-    it('BSD check', function() {
-        var data = license('asdf\nRedistribution and use in source and binary forms, with or without\nasdf\n');
+    it('BSD check', function () {
+        const data = license('asdf\nRedistribution and use in source and binary forms, with or without\nasdf\n');
         assert.equal(data, 'BSD*');
     });
 
-    it('BSD-Source-Code check', function() {
-        var data = license(
-            'asdf\nRedistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\nasdf\n'
+    it('BSD-Source-Code check', function () {
+        const data = license(
+            'asdf\nRedistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\nasdf\n',
         );
         assert.equal(data, 'BSD-Source-Code*');
     });
 
-    it('BSD word check', function() {
-        var data = license('asdf\nasdf\nBSD\nasdf\n');
+    it('BSD word check', function () {
+        const data = license('asdf\nasdf\nBSD\nasdf\n');
         assert.equal(data, 'BSD*');
     });
 
-    it('Non-BSD word check', function() {
-        var data = license('prefixBSD\n');
+    it('Non-BSD word check', function () {
+        const data = license('prefixBSD\n');
         assert.notEqual(data, 'BSD*');
     });
 
-    it('Apache version check', function() {
-        var data = license('asdf\nasdf\nApache License Version 2\nasdf\n');
+    it('Apache version check', function () {
+        const data = license('asdf\nasdf\nApache License Version 2\nasdf\n');
         assert.equal(data, 'Apache-2.0*');
     });
 
-    it('Apache word check', function() {
-        var data = license('asdf\nasdf\nApache License\nasdf\n');
+    it('Apache word check', function () {
+        const data = license('asdf\nasdf\nApache License\nasdf\n');
         assert.equal(data, 'Apache*');
     });
 
-    it('Non-Apache word check', function() {
-        var data = license('prefixApache License\n');
+    it('Non-Apache word check', function () {
+        const data = license('prefixApache License\n');
         assert.notEqual(data, 'Apache*');
     });
 
-    it('WTF check', function() {
-        var data = license('DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE');
+    it('WTF check', function () {
+        const data = license('DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE');
         assert.equal(data, 'WTFPL*');
     });
 
-    it('WTF word check', function() {
-        var data = license('asdf\nasdf\nWTFPL\nasdf\n');
+    it('WTF word check', function () {
+        const data = license('asdf\nasdf\nWTFPL\nasdf\n');
         assert.equal(data, 'WTFPL*');
     });
 
-    it('Non-WTF word check', function() {
-        var data = license('prefixWTFPL\n');
+    it('Non-WTF word check', function () {
+        const data = license('prefixWTFPL\n');
         assert.notEqual(data, 'WTFPL*');
     });
 
-    it('ISC check', function() {
-        var data = license('asdfasdf\nThe ISC License\nasdfasdf');
+    it('ISC check', function () {
+        const data = license('asdfasdf\nThe ISC License\nasdfasdf');
         assert.equal(data, 'ISC*');
     });
 
-    it('Non-ISC word check', function() {
-        var data = license('prefixISC\n');
+    it('Non-ISC word check', function () {
+        const data = license('prefixISC\n');
         assert.notEqual(data, 'ISC*');
     });
 
-    it('ISC word check', function() {
-        var data = license('asdf\nasdf\nISC\nasdf\n');
+    it('ISC word check', function () {
+        const data = license('asdf\nasdf\nISC\nasdf\n');
         assert.equal(data, 'ISC*');
     });
 
-    it('CC0-1.0 word check', function() {
-        var data = license(
-            'The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.\n\nYou can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.\n'
+    it('CC0-1.0 word check', function () {
+        const data = license(
+            'The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.\n\nYou can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.\n',
         );
         assert.equal(data, 'CC0-1.0*');
     });
 
-    it('Public Domain check', function() {
-        var data = license('Public Domain');
+    it('Public Domain check', function () {
+        let data = license('Public Domain');
         assert.equal(data, 'Public Domain');
         data = license('public domain');
         assert.equal(data, 'Public Domain');
@@ -156,8 +156,8 @@ describe('license parser', function() {
         assert.equal(data, 'Public Domain');
     });
 
-    it('License at URL check', function() {
-        var data = license('http://example.com/foo');
+    it('License at URL check', function () {
+        let data = license('http://example.com/foo');
         assert.equal(data, 'Custom: http://example.com/foo');
         data = license('See license at http://example.com/foo');
         assert.equal(data, 'Custom: http://example.com/foo');
@@ -165,33 +165,33 @@ describe('license parser', function() {
         assert.equal(data, 'Custom: https://example.com/foo');
     });
 
-    it('License at file check', function() {
-        var data = license('See license in LICENSE.md');
+    it('License at file check', function () {
+        let data = license('See license in LICENSE.md');
         assert.equal(data, 'Custom: LICENSE.md');
         data = license('SEE LICENSE IN LICENSE.md');
         assert.equal(data, 'Custom: LICENSE.md');
     });
 
-    it('Check for null', function() {
-        var data = license('this is empty, hi');
+    it('Check for null', function () {
+        const data = license('this is empty, hi');
         assert.equal(data, null);
     });
 
-    describe('SPDX licenses', function() {
-        it('should parse a basic SPDX license', function() {
+    describe('SPDX licenses', function () {
+        it('should parse a basic SPDX license', function () {
             var data = ['MIT', 'LGPL-2.0', 'Apache-2.0', 'BSD-2-Clause'];
-            data.forEach(function(licenseType) {
+            data.forEach(function (licenseType) {
                 assert.equal(license(licenseType), licenseType);
             });
         });
 
-        it('should parse more complicated license expressions', function() {
+        it('should parse more complicated license expressions', function () {
             var data = [
                 '(GPL-2.0+ WITH Bison-exception-2.2)',
                 'LGPL-2.0 OR (ISC AND BSD-3-Clause+)',
                 'Apache-2.0 OR ISC OR MIT',
             ];
-            data.forEach(function(licenseType) {
+            data.forEach(function (licenseType) {
                 assert.equal(license(licenseType), licenseType);
             });
         });
